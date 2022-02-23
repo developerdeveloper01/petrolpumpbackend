33112b03f7f1dfd9105585e45586bcd8e7e373ba
developerdeveloper01 <deepakdevelopersveltosest01@gmail.com>
2022-02-23 18:43:39 +0530

    asdjkf

--- routes/dealershipform.js
+++ routes/dealershipform.js
@@ -16,8 +16,8 @@ router.post("/dealer/signupsendotp", signupsendotp);
 router.post("/dealer/verifyotp", verifyotp);
 router.post("/dealer/addeditbasicdealershipform/:id", addeditbasicdealershipform);
 router.post("/dealer/addeditadvancedealershipform/:id", addeditadvancedealershipform);
-router.get("/user/viewonedealershipform/:id", viewonedealershipform);
+router.get("/dealer/viewonedealershipform/:id", viewonedealershipform);
 router.get("/dealer/alldealers", alldealers);
-router.get("/user/deletedealershipform/:id", deletedealershipform);
+router.get("/dealer/deletedealershipform/:id", deletedealershipform);
 
 module.exports = router;
