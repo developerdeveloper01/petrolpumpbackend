a0c377a90da7e04686b3b956e0450250cd3652fa
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-03-17 13:18:46 +0530

    payment mode

--- controllers/payment.js
+++ controllers/payment.js
@@ -39,14 +39,14 @@ exports.allpayment = async (req, res) => {
       .find().populate([{
         path:"select_bank",
         select:"name_of_bank"
-      }]).populate("dealer_name1").populate('select_mode')
+      }]).populate("dealer_name1")
       .sort({ sortorder: 1 })
       .then((data) => resp.successr(res, data))
       .catch((error) => resp.errorr(res, error));
   };
   exports.getonepayment = async (req, res) => {
     await Payment
-      .findOne({ _id: req.params.id }).populate("dealer_name1").populate('select_mode').populate([{
+      .findOne({ _id: req.params.id }).populate("dealer_name1").populate([{
         path:"select_bank",
         select:"name_of_bank"
       }])
@@ -64,7 +64,7 @@ exports.allpayment = async (req, res) => {
           $set: req.body,
         },
         { new: true }
-      ).populate("dealer_name1").populate('select_mode').populate([{
+      ).populate("dealer_name1").populate([{
         path:"select_bank",
         select:"name_of_bank"
       }])
