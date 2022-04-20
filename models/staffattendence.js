d09569fab3d8f5987a95c4d29aa71178cc91f659
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-04-20 16:58:25 +0530

    commit

--- models/staffattendence.js
+++ models/staffattendence.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 
 const staffattendence = new Schema(
   {
+    dealer_Id: { type: mongoose.Schema.Types.ObjectId, ref: "dealerform" },
     name_of_staff: { type: String, require: true },
-    designation: { type: String, },
+    designation: { type: String },
     attendence_marking: { type: String, require: true },
     leave_taken: { type: Number },
     leave_available: { type: Number },
-    
   },
   { timestamps: true }
 );
--- routes/staffattendence.js
+++ routes/staffattendence.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const {
-    addattendence,
-    allatendence,
-    getoneatendence,
-    deleteatendence,
-    updateatendence,
-  } = require("../controllers/staffattendence");
- router.post("/dealer/addattendence",addattendence);
+  addattendence,
+  allatendence,
+  getoneatendence,
+  deleteatendence,
+  updateatendence,
+  allatendenceApp,
+} = require("../controllers/staffattendence");
+router.post("/dealer/addattendence", addattendence);
 router.get("/dealer/allatendence", allatendence);
 router.post("/dealer/updateatendence/:id", updateatendence);
 
 router.get("/dealer/getoneatendence/:id", getoneatendence);
-
+router.get("/dealer/allatendenceApp/:dealer_Id", allatendenceApp);
 router.get("/dealer/deleteatendence/:id", deleteatendence);
 
 module.exports = router;
