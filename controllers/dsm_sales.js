a69b13afd232d120cfae6457ed366ab40346f66a
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-05-10 10:53:38 +0530

    dsm sales dash bord

--- controllers/dsm_sales.js
+++ controllers/dsm_sales.js
@@ -177,5 +177,65 @@ exports.adddsmsales = async (req, res) => {
       record.push(element.ms_testing);
       record.push(element.ms_own_use);
     }
+    for (const i of bothrecord) {
+      ms_sales.push(i.ms_sales);
+      ms_testing.push(i.ms_testing);
+      ms_own_use.push(i.ms_own_use);
+      hsd_sales.push(i.hsd_sales);
+      hsd_testing.push(i.hsd_testing);
+      hsd_own_use.push(i.hsd_own_use);
+    }
+
+    console.log(ms_sales);
+    console.log(ms_testing);
+    console.log(ms_own_use);
+    let summs_sales = _.sum(ms_sales);
+    console.log(summs_sales);
+    let summs_testing = _.sum(ms_testing);
+    console.log(summs_testing);
+    let summs_own_use = _.sum(ms_own_use);
+    console.log(summs_own_use);
+    let netsalems = summs_sales - summs_testing - summs_own_use;
+    console.log("netsalems", netsalems);
+    let netamtms = rspms * netsalems;
+    console.log("netamtms", netamtms);
+
+    ///hsd
+    console.log(hsd_sales);
+    console.log(hsd_testing);
+    console.log(hsd_own_use);
+    let sumhsd_sales = _.sum(hsd_sales);
+    console.log(sumhsd_sales);
+    let sumhsd_testing = _.sum(hsd_testing);
+    console.log(sumhsd_testing);
+    let sumhsd_own_use = _.sum(hsd_own_use);
+    console.log(sumhsd_own_use);
+    let netsalehsd = sumhsd_sales - sumhsd_testing - sumhsd_own_use;
+    console.log("netsalehsd", netsalehsd);
+    let netamthsd = rsphsd * netsalehsd;
+    console.log("netamthsd", netamthsd);
+    let data = {
+      dealer_Id: dealer_Id,
+      date: date,
+      product: product,
+      dsm: record,
+
+      //ms
+      metersales_ms: summs_sales,
+      testing_ms: summs_testing,
+      ownuse_ms: summs_own_use,
+      netslesltr_ms: netsalems,
+      netsalesAmount_ms: netamtms,
+      //hsd
+      metersales_hsd: sumhsd_sales,
+      ownuse_hsd: sumhsd_own_use,
+      testing_hsd: sumhsd_testing,
+      netslesltr_hsd: netsalehsd,
+      netsalesAmount_hsd: netamthsd,
+    };
+    res.status(400).json({
+      status: true,
+      data: data,
+    });
   }
 };
