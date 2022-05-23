d175da9477423018dcb774007d749c4bbcfbe58d
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-05-23 14:23:58 +0530

    commit

--- models/lubestock.js
+++ models/lubestock.js
@@ -4,19 +4,18 @@ const Schema = mongoose.Schema;
 
 const lubestock = new Schema(
   {
-    dealer_name1:{type: mongoose.Schema.Types.ObjectId,ref:"dealerform"},
+    dealer_name1: { type: mongoose.Schema.Types.ObjectId, ref: "dealerform" },
     date: { type: String, require: true },
-    grade: { type: Number, },
+    grade: { type: String },
     opening_stock: { type: Number },
     rsp: { type: Number },
-    purchase_price: {type: Number},
-    selling_price_maintained:{type: Number},
-    no_of_pieces:{type: Number},
-    amount_before_tax:{type: Number},
-    cgst:{type: Number},
-    sgst:{type: Number},
-    final_inventory:{type: Number}
-    
+    purchase_price: { type: Number },
+    selling_price_maintained: { type: Number },
+    no_of_pieces: { type: Number },
+    amount_before_tax: { type: Number },
+    cgst: { type: Number },
+    sgst: { type: Number },
+    final_inventory: { type: Number },
   },
   { timestamps: true }
 );
